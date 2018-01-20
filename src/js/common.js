cb0666d675272a626b3aff309ac586e6f268dd96
Ruifeng Xu <ruifeng.xu.23@gmail.com>
2018-01-20 10:38:06 +0100

    Added support to change font size

--- src/js/common.js
+++ src/js/common.js
@@ -1,1 +1,48 @@
 import '../scss/common.scss';
+
+/**
+ * Increase or decrease the font size
+ * @param {object} event - The DOM event
+ * @param {boolean} increase - Whether to increase or decrease the font size
+ */
+function updateFontSize(event, increase) {
+    let html = document.getElementById('root');
+    let size = parseFloat(html.style['font-size']);
+    if (isNaN(size)) {
+        size = 1.0;
+    }
+
+    if (increase && size < 3.0) {
+        size += 0.1;
+    } else if (!increase && size > 0.5) {
+        size -= 0.1;
+    }
+
+    html.style['font-size'] = size + 'rem';
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document
+        .getElementById('font-increase-button')
+        .addEventListener('click', function(event) {
+            updateFontSize(event, true);
+        }, false);
+
+    document
+        .getElementById('font-decrease-button')
+        .addEventListener('click', function(event) {
+            updateFontSize(event, false);
+        }, false);
+
+    document
+        .getElementById('font-increase-button-1')
+        .addEventListener('click', function(event) {
+            updateFontSize(event, true);
+        }, false);
+
+    document
+        .getElementById('font-decrease-button-1')
+        .addEventListener('click', function(event) {
+            updateFontSize(event, false);
+        }, false);
+}, false);
