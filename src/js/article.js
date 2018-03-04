bc248748c84dc8655d2a4e2770f2df542403d0a4
Ruifeng Xu <ruifeng.xu.23@gmail.com>
2018-03-04 10:17:31 +0100

    Improved control buttons

--- src/js/article.js
+++ src/js/article.js
@@ -38,9 +38,13 @@ function setupCustomVideoControls() {
     let videoContainer = document.getElementById('video-container');
     let video = document.getElementById('video-player');
     let play = document.getElementById('video-play');
-    let mute = document.getElementById('video-mute');
     let progress = document.getElementById('video-progress');
     let progressBar = document.getElementById('video-progress-bar');
+    let skipAhead = document.getElementById('video-skip-ahead');
+    let goBack = document.getElementById('video-go-back');
+    let volume = document.getElementById('video-volume');
+    let mute = document.getElementById('video-mute');
+    let caption = document.getElementById('video-caption');
     let fullscreen = document.getElementById('video-fullscreen');
 
     play.addEventListener('click', function(event) {
@@ -51,11 +55,7 @@ function setupCustomVideoControls() {
             video.pause();
             play.innerHTML = 'Play';
         }
-    }, false);
-
-    mute.addEventListener('click', function(event) {
-        mute.innerHTML = video.muted ? 'Mute' : 'Unmute';
-        video.muted = !video.muted;
+        play.setAttribute('aria-label', play.innerHTML);
     }, false);
 
     video.addEventListener('click', function(event) {
@@ -63,14 +63,12 @@ function setupCustomVideoControls() {
     });
 
     video.addEventListener('loadedmetadata', function() {
-        progress.setAttribute('max', video.duration);
+        progressBar.setAttribute('aria-valuemax', video.duration);
     });
 
     video.addEventListener('timeupdate', function() {
-        if (!progress.getAttribute('max')) {
-            progress.setAttribute('max', video.duration);
-        }
-        progress.value = video.currentTime;
+        progressBar.setAttribute('aria-valuemax', video.duration);
+        progressBar.setAttribute('aria-valuenow', video.currentTime);
         progressBar.style.width =
             Math.floor((video.currentTime / video.duration) * 100) + '%';
     });
@@ -79,6 +77,46 @@ function setupCustomVideoControls() {
         video.currentTime = (event.pageX - progress.offsetLeft) /
             progress.offsetWidth * video.duration;
     });
+
+    skipAhead.addEventListener('click', function(event) {
+        if (video.currentTime + 10 < video.duration) {
+            video.currentTime += 10;
+        } else {
+            video.currentTime = video.duration;
+        }
+    }, false);
+
+    goBack.addEventListener('click', function(event) {
+        if (video.currentTime - 10 > 0) {
+            video.currentTime -= 10;
+        } else {
+            video.currentTime = 0;
+        }
+    }, false);
+
+    volume.addEventListener('change', function(event) {
+        video.volume = volume.value / 10.0;
+    }, false);
+
+    mute.addEventListener('click', function(event) {
+        video.muted = !video.muted;
+        mute.innerHTML = video.muted ? 'Unmute' : 'Mute';
+        mute.setAttribute('aria-label', mute.innerHTML);
+    });
+
+    video.textTracks[0].mode = 'hidden';
+
+    caption.addEventListener('click', function(event) {
+        if (video.textTracks[0].mode === 'hidden') {
+            video.textTracks[0].mode = 'showing';
+            caption.innerHTML = 'CC Off';
+            caption.setAttribute('aria-label', 'Caption off');
+        } else {
+            video.textTracks[0].mode = 'hidden';
+            caption.innerHTML = 'CC On';
+            caption.setAttribute('aria-label', 'Caption on');
+        }
+    }, false);
 
     if (isFullscreenSupported()) {
         fullscreen.addEventListener('click', function(event) {
@@ -107,6 +145,7 @@ function setupCustomVideoControls() {
                 videoContainer.setAttribute('data-fullscreen', true);
                 fullscreen.innerHTML = 'Exit fullscreen';
             }
+            fullscreen.setAttribute('aria-label', fullscreen.innerHTML);
         }, false);
 
         document.addEventListener('fullscreenchange', function(event) {
